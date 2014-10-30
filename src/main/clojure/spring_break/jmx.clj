e330bb089d5da7070556329a17537ee5cc7692c0
Henrik Heine <henrikheine@web.de>
2014-10-30 20:34:19 +0000

    simplified jmx

--- src/main/clojure/spring_break/jmx.clj
+++ src/main/clojure/spring_break/jmx.clj
@@ -79,7 +79,7 @@
 
 ;; JMX Operation wrapper
 (defn fn-wrapper-of [a-fn]
-  (proxy [java.text.Format Runnable][] ;; Runnable is marker for JMX operation (vs. attribute)
+  (proxy [java.text.Format][] 
     (toString [] (format "(fn-wrapper-of %s meta=%s)" a-fn (meta a-fn)))
     (parseObject [a-str]
       (let [fmt "+++ Calling (%s [meta=%s] %s)"
@@ -109,6 +109,7 @@
    (str "Enter as many clojure forms as there are parameters"
         "to the clojure function being exposed via JMX.")))
 
+;; OK
 (defn make-model-mbean-operation-info []
   (javax.management.modelmbean.ModelMBeanOperationInfo.
    "parseObject" ;; method name fits the type/class of fn-wrapper-of
@@ -118,105 +119,21 @@
    javax.management.MBeanOperationInfo/ACTION_INFO
    nil)) ;; descriptor
 
-#_ (defn make-getter [ref-wrapper]
-  (let [getter (-> ref-wrapper
-                   (.getClass)
-                   (.getMethod "invoke"
-                               (into-array Class [])))]
-    (.println System/out (format "!!! getter = %s" getter))
-    getter))
-
-(defn make-getter [ref-wrapper]
-  (let [getter (-> ref-wrapper
-                   (.getClass)
-                   (.getMethod "toString"
-                               (into-array Class [])))]
-    (.println System/out (format "!!! getter = %s" getter))
-    getter))
-
-(defn __make-getter [a-ref]
-  (let [getter-proxy
-        (proxy [clojure.lang.IFn][]
-          (invoke []
-            (.println System/out (format "getter called: %s" @a-ref))
-            @a-ref))
-        getter (-> getter-proxy
-                   (.getClass)
-                   (.getMethod "invoke"
-                               (into-array Class [])))]
-    getter))
-    
-(defn make-setter [ref-wrapper]
-  (let [setter (-> ref-wrapper
-                   (.getClass)
-                   (.getMethod "invoke"
-                               (into-array Class [Object])))]
-    (.println System/out (format "!!! setter = %s" setter))
-    setter))
-
-(defn ___make-setter [a-ref]
-  (let [setter-proxy nil
-        setter nil]
-    setter))
-
-#_ (defn make-model-mbean-attribute-info [bean-obj]
-  (let [getter (make-getter bean-obj)
-        setter nil #_ (make-setter bean-obj)
-        descriptor nil #_
-        (javax.management.modelmbean.DescriptorSupport.
-         (into-array
-          String
-          [#_ "openType=javax.management.openmbean.SimpleType(name=java.lang.String)"
-           "originalType=java.lang.Object"]))]
-    (.println System/out (format "!!! getter returns %s"
-                                 (.invoke getter bean-obj (into-array Object []))))
-    (.invoke setter bean-obj (into-array Object ["foo"]))
-    (.println System/out (format "!!! getter returns %s"
-                                 (.invoke getter bean-obj (into-array Object []))))
-    (javax.management.modelmbean.ModelMBeanAttributeInfo.
-     "a name"
-     "a descrption"
-     getter
-     nil #_ setter
-     descriptor)))
-
-(defn make-model-mbean-attribute-info [bean-obj]
-  (let [getter (make-getter bean-obj)
-        setter nil #_ (make-setter bean-obj)
-        info (javax.management.modelmbean.ModelMBeanAttributeInfo.
-              "Foo"
-              "a descrption"
-              getter
-              setter)
-        desc (.getDescriptor info)]
-    (.println System/out (format "!!! getter returns %s"
-                                 (.invoke getter bean-obj (into-array Object []))))
-    ;;(.invoke setter bean-obj (into-array Object ["foo"]))
-    (.println System/out (format "!!! getter returns %s"
-                                 (.invoke getter bean-obj (into-array Object []))))
-    (.setField desc "getMethod" (.getName getter))
-    ;;(.setField desc "currencyTimeLimit" (str Integer/MAX_VALUE))
-    #_ (.setField desc "setMethod" (.getName setter))
-    (.setDescriptor info desc)
-    info))
-
+;; OK
 (defn make-model-mbean-info [bean-obj bean-name]
-  (let [is-fn (instance? java.text.Format bean-obj)
-        _ (.println System/out
-                    (format
-                     "+++ making model-mbean-info for bean-obj = %s  is-fn = %s"
-                     bean-obj is-fn))]
-    (javax.management.modelmbean.ModelMBeanInfoSupport.
-     ;;"classname ignored!"
-     (.getName (.getClass bean-obj))
-     "description ignored"
-     (into-array javax.management.modelmbean.ModelMBeanAttributeInfo
-                 (if-not is-fn [(make-model-mbean-attribute-info bean-obj)] []))
-     (into-array javax.management.modelmbean.ModelMBeanConstructorInfo [])
-     (into-array javax.management.modelmbean.ModelMBeanOperationInfo
-                 (if is-fn [(make-model-mbean-operation-info)] []))
-     (into-array javax.management.modelmbean.ModelMBeanNotificationInfo []))))
+  (.println System/out
+            (format
+             "+++ making model-mbean-info for bean-obj = %s"
+             bean-obj))
+  (javax.management.modelmbean.ModelMBeanInfoSupport.
+   "classname ignored!"
+   "description ignored"
+   (into-array javax.management.modelmbean.ModelMBeanAttributeInfo [])
+   (into-array javax.management.modelmbean.ModelMBeanConstructorInfo [])
+   (into-array [(make-model-mbean-operation-info)])
+   (into-array javax.management.modelmbean.ModelMBeanNotificationInfo [])))
 
+;; OK
 (defn mbean-info-assembler [pred]
   (proxy [org.springframework.jmx.export.assembler.AutodetectCapableMBeanInfoAssembler][]
     (includeBean [bean-class bean-name]
